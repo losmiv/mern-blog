411ef85208d39965545acb56de96e15e9bb5f718
vm34 <fbueller2234@protonmail.com>
2023-05-06 02:10:20 +0200

    upload file using multer

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 const app = express();
 
@@ -16,6 +17,24 @@ const options = {
 app.use(cors(options));
 app.use(express.json());
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '../client/public/upload');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname);
+    }
+});
+
+const upload = multer({ dest: 'uploads/' });
+
+add.post('/upload', upload.single('file'), function (req, res) {
+    file = req.file;
+    res.status(200).json(file.filename);
+});
+
+
 app.use("/api/posts", postRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,15 +1,70 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { useNavigate } from 'react-router-dom';
 
 const Write = () => {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState(state?.title || '');
+  const [title, setTitle] = useState(state?.desc || '');
+  const [file, setFile] = useState(null);
+  const [cat, setCat] = useState(state?.cat || '');
+
+  const navigate = useNavigate();
+
+
+  const upload = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      const res = await axios.post('/upload', formData);
+      return res.data;
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const imgUrl = await upload();
+
+    try {
+      state
+        ? await axios.put(`/posts/${state.id}`, {
+          title,
+          desc: value,
+          cat,
+          img: file ? imgUrl : "",
+        })
+        : await axios.post(`/posts/`, {
+          title,
+          desc: value,
+          cat,
+          img: file ? imgUrl: "",
+          date: moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+        });
+        navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className='add'>
       <div className='content'>
-        <input type='text' placeholder='Title' />
+        <input
+          type='text'
+          placeholder='Title'
+          onChange={e=>setTitle(e.target.value)}
+        />
         <div className='editorContainer'>
-          <ReactQuill theme='snow' value={value} onChange={setValue} />
+          <ReactQuill
+            className='editor'
+            theme='snow'
+            value={value}
+            onChange={setValue}
+          />
         </div>
       </div>
       <div className='menu'>
@@ -21,33 +76,39 @@ const Write = () => {
           <span>
             <b>Visibility: </b> Public
           </span>
-          <input style={{display:'none'}} type='file' id='file' name='' />
+          <input
+            style={{display:'none'}}
+            type='file'
+            id='file'
+            name=''
+            onChange={e=>setFile(e.target.files)}
+          />
           <label className='file' htmlFor='file'>Upload Image</label>
           <div className='buttons'>
             <button>Save as a draft</button>
-            <button>Update</button>
+            <button onClick={handleClick}>Publish</button>
           </div>
         </div>
         <div className='item'>
           <h1>Category</h1>
           <div className='cat'>
-            <input type='radio' name='cat' value='art' id='art' />
+            <input type='radio' checked={cat === art} name='cat' value='art' id='art' onChange={e=>setCat(e.target.value)} />
             <label htmlFor='art'>Art</label>
           </div>
           <div className='cat'>
-            <input type='radio' name='cat' value='science' id='science' />
+            <input type='radio' checked={cat === science} name='cat' value='science' id='science' onChange={e=>setCat(e.target.value)} />
             <label htmlFor='science'>Science</label>
           </div>
           <div className='cat'>
-            <input type='radio' name='cat' value='tech' id='tech' />
+            <input type='radio' checked={cat === tech} name='cat' value='tech' id='tech' onChange={e=>setCat(e.target.value)} />
             <label htmlFor='tech'>Tech</label>
           </div>
           <div className='cat'>
-            <input type='radio' name='cat' value='economy' id='economy' />
+            <input type='radio' checked={cat === economy} name='cat' value='economy' id='economy' onChange={e=>setCat(e.target.value)} />
             <label htmlFor='economy'>Economy</label>
           </div>
           <div className='cat'>
-            <input type='radio' name='cat' value='politics' id='politics' />
+            <input type='radio' checked={cat === politics} name='cat' value='politics' id='politics' onChange={e=>setCat(e.target.value)} />
             <label htmlFor='politics'>Politics</label>
           </div>
         </div>
